6b6aeff595a229d6c8f410726a573a182fcf8cca
Devsk001 <srik3713@gmail.com>
2023-10-09 08:00:17 +0530

    Projects css finished

--- js/_projects.js
+++ js/_projects.js
@@ -1,1 +1,19 @@
 console.log('Welcome to SonthaBoomi !');
+
+
+// todo   Get The Dom Element for expand function
+
+
+
+function expand(ExpandDiv) {
+    //getting the division
+    const expand_div = document.getElementById(`${ExpandDiv}`);
+    console.log(expand_div);
+    expand_div.style.display = 'block';
+    expand_div.classList= 'animation';
+    console.log(expand_div.classList);
+}
+
+
+
+
